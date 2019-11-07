642e932a4207f275467cd244adfe740869ee25bf
Kumovic Nenad <nenad.kumovic@it4communication.ch>
2019-11-07 16:14:52 +0100

    Updates

---
+++ precache-manifest.6efb9bfd84d4e489f82e7d8c9c6ddaca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73a948684995a6818294328dc7fb60f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7317497d4257f3547383",
+    "url": "/robofriends/static/css/main.db8a8840.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "7317497d4257f3547383",
+    "url": "/robofriends/static/js/main.5b2eb4c1.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
